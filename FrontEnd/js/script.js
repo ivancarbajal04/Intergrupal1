10f8c4001aa005ab791b1e684f35c741d0ee0596
Ivan <ivanncarbajal@hotmail.com>
2022-11-28 11:44:08 -0300

    Primer commit

---
+++ FrontEnd/js/script.js
@@ -0,0 +1,19 @@
+// let edad = prompt("Ingresa tu edad")
+
+// edad = parseInt(edad)
+
+// if(edad < 18){
+//     console.log("No puede pasar al bar")
+// }
+// else if(edad > 60){
+//     console.log("No podes pasar")
+// }
+// else{
+//     console.log("Podes pasar")
+// }
+
+for(i = 0; i <= 5; i++){
+    console.log("Hello, world!")
+}
+
+
